ed12e2b00e3f1d36a6936a5fae9c53f71a47ff30
navind062g <62036788+navind062g@users.noreply.github.com>
2021-10-22 10:45:45 -0500

    Changes Done

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,6 +6,7 @@ class SearchBar extends React.Component {
         super(props);
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
 
         this.setState({
             searchTerm: ''
@@ -19,10 +20,16 @@ class SearchBar extends React.Component {
         this.setState({searchTerm: evt.target.value});
     }
 
+    handleKeyPress(event) {
+        if(event.key === 'Enter') {
+            this.search();
+        }
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress}/>
                 <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
